27155e11377c7fa5d6d526f0b7ffc00cbc6c8e75
kiberbulka <etozheolya385@mail.ru>
2024-11-04 14:36:57 +0500

    Добавлен экран пользователя

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Olya on 04.11.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+
+    
+    @IBOutlet private var descriptionLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var avatarImageView: UIImageView!
+    
+    
+    @IBOutlet private var logoutButton: UIButton!
+    
+    @IBAction private func didTapLogoutButton(_ sender: Any) {
+        
+    }
+    
+
+}
